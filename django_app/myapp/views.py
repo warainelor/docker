2f4cc5ba66a01ddfbbd3ba235de457dbc861a64b
= <yjcrwf@ya.ru>
2025-01-19 15:42:00 +0300

    add django_app

---
+++ django_app/myapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HelloWorldView(APIView):
+    def get(self, request):
+        return Response({"message": "Hello, world!"})
